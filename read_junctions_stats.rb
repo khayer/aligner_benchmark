ef3fe5f21cb03d515f6f0034e2fc17bf489e1643
Katharina Hayer <katharinaehayer@gmail.com>
2015-07-20 10:37:32 -0400

    added summary for junctions

---
+++ read_junctions_stats.rb
@@ -0,0 +1,38 @@
+###
+#
+# IN:
+# [hayer@consign aligner_benchmark]$ ls */junctions_stats.txt
+#     gsnap/junctions_stats.txt  hisat/junctions_stats.txt  mapsplice2/junctions_stats.txt
+# OUT: Summary
+#
+###
+all = []
+ARGV[0..-1].each do |arg|
+  info = []
+  info << arg.split("/")[0]
+  File.open(arg).each do |line|
+    line.chomp!
+    fields = line.split(" ")
+    info << fields[-1]
+  end
+  all << info
+end
+
+#puts "aligner\ttotal_number_of_bases_of_reads\taccuracy over all bases\taccuracy over uniquely aligned bases"
+
+File.open(ARGV[0]).each_with_index do |line,j|
+  line.chomp!
+  fields = line.split(" = ")
+  case j
+  when 0
+    print "Aligner\t"
+  else
+    print "#{fields[0]}\t"
+  end
+  res = []
+  for i in 0...ARGV.length
+    res << all[i][j+1]
+  end
+  print res.join("\t")
+  print "\n"
+end
