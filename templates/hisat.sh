1bfa3b450dc62d15e8d09ebcd2cebc5b3eabe102
Katharina Hayer <katharinaehayer@gmail.com>
2015-08-12 14:36:22 -0400

    hitting the pause button

---
+++ templates/hisat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+#BSUB -J hisat_stats              # LSF job name
+#BSUB -o hisat_stats.%J.out       # Name of the job output file
+#BSUB -e hisat_stats.%J.error     # Name of the job error file
+#BSUB -M 18432
+
+cd <%= @stats_path %>
+
+ln -s <%= @tool_result_path %>/output.sam fixed.sam
+perl <%= @aligner_benchmark %>/perl_scripts/compare2truth.pl <%= @cig_file %> fixed.sam -noHtag > comp_res.txt
+perl <%= @aligner_benchmark %>/perl_scripts/sam2junctions.pl fixed.sam > inferred_junctions.txt
+perl <%= @aligner_benchmark %>/perl_scripts/compare_junctions_in_simulated_to_INFERRED_junctions.pl <%= @transcripts %> <%= @junctions_crossed %> inferred_junctions.txt junctions > junctions_stats.txt
