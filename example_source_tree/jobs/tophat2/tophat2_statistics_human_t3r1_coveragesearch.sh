d2623398cb8ccb461cef91b5e55a054e55584a30
Katharina Hayer <katharinaehayer@gmail.com>
2015-08-12 12:42:53 -0400

    hitting the pause button

--- example_source_tree/jobs/tophat2/tophat2_statistics_human_t3r1_coveragesearch.sh
+++ example_source_tree/jobs/tophat2/tophat2_statistics_human_t3r1_coveragesearch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 #BSUB -J tophat2_stats              # LSF job name
 #BSUB -o tophat2_stats.%J.out       # Name of the job output file
 #BSUB -e tophat2_stats.%J.error     # Name of the job error file
