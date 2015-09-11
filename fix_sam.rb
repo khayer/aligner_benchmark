a221fc9db50a990929977062b7858f225e22373a
Katharina Hayer <katharinaehayer@gmail.com>
2015-09-11 14:07:58 -0400

    hitting the pause button

--- fix_sam.rb
+++ fix_sam.rb
@@ -205,7 +205,7 @@ end
 old_name =~ /(\d+)/
 old_num = $1.to_i
 num = 10000000
-while !(num <= old_num-1)
+while !(num <= old_num)
   add_empty_lines(old_num)
   old_name = "seq.#{old_num+1}"
   old_num += 1
