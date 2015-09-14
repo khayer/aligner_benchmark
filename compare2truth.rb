7539875237f814ad2deec158cb7086b46efe22f9
Katharina Hayer <katharinaehayer@gmail.com>
2015-09-14 16:27:03 -0400

    still to do 'base by base' case

--- compare2truth.rb
+++ compare2truth.rb
@@ -36,7 +36,8 @@ end
 def setup_options(args)
   options = {
     :loglevel => "error",
-    :debug => false
+    :debug => false,
+    :read_length => nil
   }
 
   opt_parser = OptionParser.new do |opts|
@@ -70,11 +71,11 @@ def setup_options(args)
     #  options[:out_file] = anno_file
     #end
 
-    #opts.on("-s", "--species [String]",
-    #  :REQUIRED,String,
-    #  "Spiecies, Default: human") do |s|
-    #  options[:species] = s
-    #end
+    opts.on("-r", "--read_length [INT]",
+      :REQUIRED,Integer,
+      "read length, if not specified it will be taken from cig file") do |s|
+      options[:species] = s
+    end
 
     opts.on("-v", "--verbose", "Run verbosely") do |v|
       options[:log_level] = "info"
@@ -141,17 +142,23 @@ deletions_called_correctly: #{@deletions_called_correctly}
 end
 
 
-def files_valid?(truth_cig,sam_file)
+def files_valid?(truth_cig,sam_file,options)
   l = `grep ^seq #{truth_cig} | head -1`
+  l.chomp!
   l =~ /seq.(\d+)/
   first_truth = $1
+  l =~ /\t([^\t]+)$/;
+  options[:read_length] ||= $1.length;
   l = `tail -1 #{truth_cig}`
+  l.chomp!
   l =~ /seq.(\d+)/
   last_truth = $1
   l = `grep ^seq #{sam_file} | head -1`
+  l.chomp!
   l =~ /seq.(\d+)/
   first_sam = $1
   l = `tail -1 #{sam_file}`
+  l.chomp!
   l =~ /seq.(\d+)/
   last_sam = $1
   unless last_sam == last_truth && first_sam == first_truth
@@ -160,7 +167,54 @@ def files_valid?(truth_cig,sam_file)
   end
 end
 
-def process(current_group, cig_group)
+def comp_base_by_base(s_sam,c_cig,stats)
+
+end
+
+def process(current_group, cig_group, stats,options)
+  cig_group.each do |l|
+    l = l.split("\t")
+    k = l[4].dup
+    inserts = 0
+    while k =~ /(\d+)I/
+      inserts = inserts+$1.to_i
+      k.sub!(/(\d+)I/,"")
+    end
+    stats.total_number_of_bases_in_true_insertions += inserts
+    k = l[4].dup
+    deletions = 0
+    while k =~ /(\d+)D/
+      deletions = deletions+$1.to_i
+      k.sub!(/(\d+)D/,"")
+    end
+    stats.total_number_of_bases_in_true_deletions += deletions
+    stats.total_number_of_bases_of_reads += options[:read_length]
+    if current_group.length > 2
+      stats.total_number_of_bases_aligned_ambiguously += options[:read_length]
+    else
+      current_group.each do |s|
+        s = s.split("\t")
+        next unless l[0] == s[0]
+        if s[2] == "*"
+          stats.total_number_of_bases_unaligned += options[:read_length]
+        else
+          if s[2] != l[1]
+            stats.total_number_of_bases_aligned_incorrectly += options[:read_length]
+          else
+            if s[3] == l[2] && s[5] == l[4]
+              stats.total_number_of_bases_aligned_correctly += options[:read_length]
+              stats.insertions_called_correctly += inserts
+              stats.total_number_of_bases_called_insertions += inserts
+              stats.deletions_called_correctly += deletions
+              stats.total_number_of_bases_called_deletions += deletions
+            else
+              comp_base_by_base(s,l,stats)
+            end
+          end
+        end
+      end
+    end
+  end
   $logger.debug(current_group.length)
   $logger.debug(cig_group[0])
 end
@@ -184,7 +238,7 @@ def compare(truth_cig, sam_file, options)
     else
       cig_group << truth_cig_handler.readline.chomp
       cig_group << truth_cig_handler.readline.chomp
-      process(current_group, cig_group,stats)
+      process(current_group, cig_group,stats,options)
       current_num = $1
       current_group = []
       cig_group = []
@@ -193,7 +247,7 @@ def compare(truth_cig, sam_file, options)
   end
   cig_group << truth_cig_handler.readline.chomp
   cig_group << truth_cig_handler.readline.chomp
-  process(current_group, cig_group,stats)
+  process(current_group, cig_group,stats,options)
   stats
 end
 
@@ -201,8 +255,9 @@ def run(argv)
   options = setup_options(argv)
   truth_cig = argv[0]
   sam_file = argv[1]
+  $logger.info("Options are #{options}")
 
-  files_valid?(truth_cig,sam_file)
+  files_valid?(truth_cig,sam_file,options)
   stats = compare(truth_cig, sam_file, options)
   stats.process()
   puts stats
