d940200fdffdfe8158de13ec36c6719d74900880
Katharina Hayer <katharinaehayer@gmail.com>
2015-09-21 09:40:25 -0400

    hitting the pause button

--- master.rb
+++ master.rb
@@ -132,9 +132,7 @@ end
 
 def check_if_results_exist(stats_path)
   File.exist?("#{stats_path}/comp_res.txt") &&
-  !File.zero?("#{stats_path}/comp_res.txt") &&
-  File.exist?("#{stats_path}/junctions_stats.txt") &&
-  !File.zero?("#{stats_path}/junctions_stats.txt")
+  !File.zero?("#{stats_path}/comp_res.txt") #&& File.exist?("#{stats_path}/junctions_stats.txt") && !File.zero?("#{stats_path}/junctions_stats.txt")
 end
 
 def get_truth_files(options, source_of_tree, dataset)
