f4437427f85319c92346bf272c1f0c6b97e3bb19
Katharina Hayer <katharinaehayer@gmail.com>
2015-09-24 16:54:38 -0400

    changed templates

--- templates/olego.sh
+++ templates/olego.sh
@@ -2,12 +2,11 @@
 #BSUB -J olego_stats              # LSF job name
 #BSUB -o olego_stats.%J.out       # Name of the job output file
 #BSUB -e olego_stats.%J.error     # Name of the job error file
-#BSUB -M 18432
 
 cd <%= @stats_path %>
 
 ln -s <%= @tool_result_path %>/output.sam output.sam
 ln -s output.sam fixed.sam
 ruby <%= @aligner_benchmark %>/compare2truth.rb <%= @cig_file %> fixed.sam > comp_res.txt
-perl <%= @aligner_benchmark %>/perl_scripts/sam2junctions.pl fixed.sam > inferred_junctions.txt
-perl <%= @aligner_benchmark %>/perl_scripts/compare_junctions_in_simulated_to_INFERRED_junctions.pl <%= @transcripts %> <%= @junctions_crossed %> inferred_junctions.txt junctions > junctions_stats.txt
+#perl <%= @aligner_benchmark %>/perl_scripts/sam2junctions.pl fixed.sam > inferred_junctions.txt
+#perl <%= @aligner_benchmark %>/perl_scripts/compare_junctions_in_simulated_to_INFERRED_junctions.pl <%= @transcripts %> <%= @junctions_crossed %> inferred_junctions.txt junctions > junctions_stats.txt
--- templates/tophat2.sh
+++ templates/tophat2.sh
@@ -2,7 +2,6 @@
 #BSUB -J tophat2_stats              # LSF job name
 #BSUB -o tophat2_stats.%J.out       # Name of the job output file
 #BSUB -e tophat2_stats.%J.error     # Name of the job error file
-#BSUB -M 18432
 
 cd <%= @stats_path %>
 
@@ -10,5 +9,5 @@ cd <%= @stats_path %>
 <%= @samtools %> view merged.bam | sort -t'.' -k 2n > output.sam
 ruby <%= @aligner_benchmark %>/fix_sam.rb output.sam > fixed.sam
 ruby <%= @aligner_benchmark %>/compare2truth.rb <%= @cig_file %> fixed.sam > comp_res.txt
-perl <%= @aligner_benchmark %>/perl_scripts/sam2junctions.pl fixed.sam > inferred_junctions.txt
-perl <%= @aligner_benchmark %>/perl_scripts/compare_junctions_in_simulated_to_INFERRED_junctions.pl <%= @transcripts %> <%= @junctions_crossed %> inferred_junctions.txt junctions > junctions_stats.txt
+#perl <%= @aligner_benchmark %>/perl_scripts/sam2junctions.pl fixed.sam > inferred_junctions.txt
+#perl <%= @aligner_benchmark %>/perl_scripts/compare_junctions_in_simulated_to_INFERRED_junctions.pl <%= @transcripts %> <%= @junctions_crossed %> inferred_junctions.txt junctions > junctions_stats.txt
