c72507dc98188cfc2422dbcee487b5535e72a9c7
Katharina Hayer <katharinaehayer@gmail.com>
2015-11-11 16:39:38 -0500

    hitting the pause button

--- master.rb
+++ master.rb
@@ -548,31 +548,53 @@ def run_hisat2(options, source_of_tree, dataset)
   end
   l = l[0]
   erubis = Erubis::Eruby.new(File.read("#{options[:aligner_benchmark]}/templates/hisat2.sh"))
-  return unless File.exist?("#{l}/output.sam")
-  options[:stats_path] = "#{options[:out_directory]}/hisat2/"
-  begin
-    Dir.mkdir(options[:stats_path])
-  rescue SystemCallError
-    if Dir.exist?(options[:stats_path])
-      logger.warn("Directory #{options[:stats_path]} exists!")
+  Dir.glob("#{l}/*").each do |p|
+    if File.directory? p
+      next unless File.exist?("#{p}/output.sam")
+      $logger.debug(p)
+      options[:stats_path] = "#{options[:out_directory]}/hisat2/#{p.split("/")[-1]}".gsub(/[()]/,"")
+      begin
+        Dir.mkdir(options[:stats_path])
+      rescue SystemCallError
+        if Dir.exist?(options[:stats_path])
+          logger.warn("Directory #{options[:stats_path]} exists!")
+        else
+          logger.error("Can't create directory #{options[:stats_path]}!")
+          raise("Trouble creating directory, log for details.")
+        end
+      end
+      options[:tool_result_path] = p
+      shell_file = "#{options[:jobs_path]}/hisat2_statistics_#{options[:species]}_#{dataset}_#{p.split("/")[-1]}.sh".gsub(/[()]/,"")
     else
-      logger.error("Can't create directory #{options[:stats_path]}!")
-      raise("Trouble creating directory, log for details.")
+      next unless p =~ /output\.sam$/
+      $logger.debug(p)
+      options[:stats_path] = "#{options[:out_directory]}/hisat2/".gsub(/[()]/,"")
+      begin
+        Dir.mkdir(options[:stats_path])
+      rescue SystemCallError
+        if Dir.exist?(options[:stats_path])
+          logger.warn("Directory #{options[:stats_path]} exists!")
+        else
+          logger.error("Can't create directory #{options[:stats_path]}!")
+          raise("Trouble creating directory, log for details.")
+        end
+      end
+      options[:tool_result_path] = p.gsub(/output\.sam$/,"")
+      shell_file = "#{options[:jobs_path]}/hisat2_statistics_#{options[:species]}_#{dataset}_default.sh"
     end
-  end
 
-  return if check_if_results_exist(options[:stats_path])
-  clean_files(options[:stats_path])
-  options[:tool_result_path] = l
-  shell_file = "#{options[:jobs_path]}/hisat2_statistics_#{options[:species]}_#{dataset}.sh"
-  o = File.open(shell_file,"w")
-  o.puts(erubis.evaluate(options))
-  o.close()
-  Dir.chdir "#{options[:jobs_path]}"
-  $logger.debug(Dir.pwd)
-  cmd = "bsub < #{shell_file}"
-  jobnumber = submit(cmd,options)
-  options[:jobs] << Job.new(jobnumber, cmd, "PEND",Dir.pwd)
+    next if check_if_results_exist(options[:stats_path])
+    clean_files(options[:stats_path])
+
+    o = File.open(shell_file,"w")
+    o.puts(erubis.evaluate(options))
+    o.close()
+    Dir.chdir "#{options[:jobs_path]}"
+    $logger.debug(Dir.pwd)
+    cmd = "bsub < #{shell_file}"
+    jobnumber = submit(cmd,options)
+    options[:jobs] << Job.new(jobnumber, cmd, "PEND",Dir.pwd)
+  end
   $logger.debug(options[:jobs])
 end
 
